c4b9dc976c679ead9fb882c24eca090ecfcb85df
vernyuy <fonchu.e.venyuy@gmail.com>
2023-12-09 08:16:52 +0100

    updates

--- lib/step-func-rest-stack.ts
+++ lib/step-func-rest-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as sfn from "aws-cdk-lib/aws-stepfunctions";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as logs from "aws-cdk-lib/aws-logs";
 import * as fs from "fs";
@@ -11,6 +12,13 @@ export class StepFuncRestStack extends cdk.Stack {
 
     const logGroup = new logs.LogGroup(this, "StepFunction");
 
+    const table = new dynamodb.Table(this, "weather-table", {
+      tableName: "weather-table",
+      partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
+      sortKey: { name: "city", type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
     const file = fs.readFileSync(__dirname + "/stepfn.asl");
     const stateMachine = new sfn.StateMachine(this, "MyStateMachine", {
       definitionBody: sfn.StringDefinitionBody.fromString(file.toString()),
@@ -26,10 +34,78 @@ export class StepFuncRestStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
 
-    const api = new apigateway.StepFunctionsRestApi(
-      this,
-      "StepFunctionsRestApi",
-      { stateMachine: stateMachine }
+    table.grantReadWriteData(stateMachine);
+
+    const stateMachineARN = stateMachine.stateMachineArn;
+
+    const RequestTemplateJson =
+      '#set($inputString = \'\')\r\n#set($includeHeaders = true)\r\n#set($includeQueryString = true)\r\n#set($includePath = true)\r\n#set($includeAuthorizer = false)\r\n#set($allParams = $input.params())\r\n#set($inputRoot=\'"httpMethod" :"\'+ $context.httpMethod+\'"\')\r\n{\r\n    "stateMachineArn": "' +
+      stateMachineARN +
+      '",\r\n\r\n    #set($inputString = "$inputString,@@body@@: $input.body")\r\n\r\n    #if ($includeHeaders)\r\n        #set($inputString = "$inputString, @@header@@:{")\r\n        #foreach($paramName in $allParams.header.keySet())\r\n            #set($inputString = "$inputString @@$paramName@@: @@$util.escapeJavaScript($allParams.header.get($paramName))@@")\r\n            #if($foreach.hasNext)\r\n                #set($inputString = "$inputString,")\r\n            #end\r\n        #end\r\n        #set($inputString = "$inputString },$inputRoot")\r\n\r\n        \r\n    #end\r\n\r\n    #if ($includeQueryString)\r\n        #set($inputString = "$inputString, @@querystring@@:{")\r\n        #foreach($paramName in $allParams.querystring.keySet())\r\n            #set($inputString = "$inputString @@$paramName@@: @@$util.escapeJavaScript($allParams.querystring.get($paramName))@@")\r\n            #if($foreach.hasNext)\r\n                #set($inputString = "$inputString,")\r\n            #end\r\n        #end\r\n        #set($inputString = "$inputString }")\r\n    #end\r\n\r\n    #if ($includePath)\r\n        #set($inputString = "$inputString, @@path@@:{")\r\n        #foreach($paramName in $allParams.path.keySet())\r\n            #set($inputString = "$inputString @@$paramName@@: @@$util.escapeJavaScript($allParams.path.get($paramName))@@")\r\n            #if($foreach.hasNext)\r\n                #set($inputString = "$inputString,")\r\n            #end\r\n        #end\r\n        #set($inputString = "$inputString }")\r\n    #end\r\n    \r\n    #if ($includeAuthorizer)\r\n        #set($inputString = "$inputString, @@authorizer@@:{")\r\n        #foreach($paramName in $context.authorizer.keySet())\r\n            #set($inputString = "$inputString @@$paramName@@: @@$util.escapeJavaScript($context.authorizer.get($paramName))@@")\r\n            #if($foreach.hasNext)\r\n                #set($inputString = "$inputString,")\r\n            #end\r\n        #end\r\n        #set($inputString = "$inputString }")\r\n    #end\r\n\r\n    #set($requestContext = "")\r\n    ## Check if the request context should be included as part of the execution input\r\n    #if($requestContext && !$requestContext.empty)\r\n        #set($inputString = "$inputString,")\r\n        #set($inputString = "$inputString @@requestContext@@: $requestContext")\r\n    #end\r\n\r\n    #set($inputString = "$inputString}")\r\n    #set($inputString = $inputString.replaceAll("@@",\'"\'))\r\n    #set($len = $inputString.length() - 1)\r\n    "input": "{$util.escapeJavaScript($inputString.substring(1,$len)).replaceAll("\\\\\'","\'")}"\r\n}';
+
+    const restApi = new apigateway.RestApi(this, "weather-api", {
+      restApiName: "weather-api",
+      deployOptions: {
+        stageName: "dev",
+        dataTraceEnabled: true,
+        tracingEnabled: true,
+        metricsEnabled: true,
+      },
+    });
+
+    restApi.root.addMethod("ANY");
+    const weather = restApi.root.addResource("weather");
+    // Create weather
+    weather.addMethod(
+      "POST",
+      apigateway.StepFunctionsIntegration.startExecution(stateMachine, {
+        passthroughBehavior: apigateway.PassthroughBehavior.WHEN_NO_MATCH,
+        requestTemplates: {
+          "application/json": RequestTemplateJson,
+        },
+      })
+    );
+    const weatherIdRoute = weather.addResource('{weatherId}')
+    weatherIdRoute.addMethod(
+      "GET",
+      apigateway.StepFunctionsIntegration.startExecution(stateMachine, {
+        passthroughBehavior: apigateway.PassthroughBehavior.WHEN_NO_MATCH,
+        requestTemplates: {
+          "application/json": RequestTemplateJson,
+        },
+      })
+    );
+    
+    weatherIdRoute.addMethod(
+      "PUT",
+      apigateway.StepFunctionsIntegration.startExecution(stateMachine, {
+        passthroughBehavior: apigateway.PassthroughBehavior.WHEN_NO_MATCH,
+        requestTemplates: {
+          "application/json": RequestTemplateJson,
+        },
+      })
+    );
+
+    
+    weatherIdRoute.addMethod(
+      "DELETE",
+      apigateway.StepFunctionsIntegration.startExecution(stateMachine, {
+        passthroughBehavior: apigateway.PassthroughBehavior.WHEN_NO_MATCH,
+        requestTemplates: {
+          "application/json": RequestTemplateJson,
+        },
+      })
+    );
+
+    
+    weather.addMethod(
+      "GET",
+      apigateway.StepFunctionsIntegration.startExecution(stateMachine, {
+        passthroughBehavior: apigateway.PassthroughBehavior.WHEN_NO_MATCH,
+        requestTemplates: {
+          "application/json": RequestTemplateJson,
+        },
+      })
     );
   }
 }
--- lib/stepfn.asl
+++ lib/stepfn.asl
@@ -10,52 +10,185 @@
       "Type" : "Choice",
       "Choices": [
         {
-          "Variable": "$.level",
-          "NumericEquals": 1,
-          "Next": "BeginnerState"
+          "Variable": "$.httpMethod",
+          "StringEquals": "POST",
+          "Next": "DynamoDB Create Weather Item"
         },
         {
-          "Variable": "$.level",
-          "NumericEquals": 2,
-          "Next": "IntermidiateState"
+          "Variable": "$.httpMethod",
+          "StringEquals": "PUT",
+          "Next": "DynamoDB Update Weather Item"
         },
         {
-          "Variable": "$.level",
-          "NumericEquals": 3,
-          "Next": "AdvanceState"
+          "Variable": "$.httpMethod",
+          "StringEquals": "DELETE",
+          "Next": "DynamoDB Delete Weather Item"
+        },
+        {
+          "Variable": "$.httpMethod",
+          "StringEquals": "GET",
+          "Next": "DynamoDB Get Weather Item"
         }
       ],
-      "Default": "DefaultState"
+      "Default": "Default"
     },
-
-    "BeginnerState": {
-      "Type" : "Pass",
-      "Result": {
-        "level": "Welcome to eCa. You are a beginner"
+    "DynamoDB Create Weather Item": {
+      "Type": "Task",
+      "Resource": "arn:aws:states:::dynamodb:putItem",
+      "Parameters": {
+        "TableName": "weather-table",
+        "Item": {
+          "id": {
+            "S.$": "$.body.id"
+          },
+          "city": {
+            "S.$": "$.body.city"
+          }
+        }
       },
-      "End": true
-    },
+      "Retry": [
+        {
+          "ErrorEquals": [
+            "States.All"
+          ],
+          "IntervalSeconds": 1,
+          "MaxAttempts": 1
+        }
+      ],
+      "Catch": [{
 
-    "IntermidiateState": {
-      "Type" : "Pass",
-      "Result": {
-        "level": "Your level is Intermidiate"
+        "ErrorEquals": [
+          "States.All"
+        ],
+        "Next": "Item fail To Create"
+      }],
+      "ResultPath": "$.dynamodbPut",
+      "Next": "Item Created"
+    },
+    "DynamoDB Get Weather Item": {
+      "Type": "Task",
+      "Resource": "arn:aws:states:::dynamodb:getItem",
+      "Parameters": {
+        "TableName": "weather-table",
+        "Key": {
+          "id": {
+            "S.$": "$.path.weatherId"
+          }
+        }
       },
-      "End": true
+      "Retry": [
+        {
+          "ErrorEquals": ["States.All"],
+          "IntervalSeconds": 1,
+          "MaxAttempts": 2
+        }
+      ],
+      "Catch": [{
+        "ErrorEquals": ["States.All"],
+        "Next": "Item Not Found"
+      }],
+      "ResultPath": "$.GetStore",
+      "Next": "Item Found"
     },
 
-    "AdvanceState": {
-      "Type" : "Pass",
-      "Result": {
-        "level": "Your level is advanced"
+    "DynamoDB Update Weather Item": {
+      "Type": "Task",
+      "Resource": "arn:aws:states:::dynamodb:updateItem",
+      "Parameters": {
+        "TableName": "weather-table",
+        "Key": {
+          "id": {
+            "S.$": "$.path.weatherId"
+          }
+        },
+        "UpdateExpression": "set city = :city",
+        "ExpressionAttributeValues": {
+          ":city": {
+            "S.$": "$.body.city"
+          }
+        },
+        "ReturnValues": "UPDATED_NEW"
       },
-      "End": true
+      "Retry": [
+        {
+          "ErrorEquals": ["States.All"],
+          "IntervalSeconds": 1,
+          "MaxAttempts": 2
+        }
+      ],
+      "Catch": [{
+        "ErrorEquals": ["States.All"],
+        "Next": "Item Not Updated"
+      }],
+      "ResultPath": "$.GetStore",
+      "Next": "Item Updated"
     },
 
-    "DefaultState": {
+    "DynamoDB Delete Weather Item": {
+      "Type": "Task",
+      "Resource": "arn:aws:states:::dynamodb:deleteItem",
+      "Parameters": {
+        "TableName": "weather-table",
+        "Key": {
+          "id": {
+            "S.$": "$.path.weatherId"
+          }
+        }
+      },
+      "Retry": [
+        {
+          "ErrorEquals": ["States.All"],
+          "IntervalSeconds": 1,
+          "MaxAttempts": 2
+        }
+      ],
+      "Catch": [{
+        "ErrorEquals": ["States.All"],
+        "Next": "Item Not Deleted"
+      }],
+      "ResultPath": "$.GetStore",
+      "Next": "Item Deleted"
+    },
+
+    "Item fail To Create":{
       "Type": "Fail",
-      "Error": "DefaultStateError",
-      "Cause": "No Matches!"
+      "Error": "Item Create Fail",
+      "Cause": "Dynamodb error"
+    },
+    "Item Created":{
+      "Type": "Pass",
+      "End": true
+    },
+    "Item Found":{
+      "Type": "Pass",
+      "End": true
+    },
+    "Item Not Found":{
+      "Type": "Fail",
+      "Error": "Item Create Fail",
+      "Cause": "Dynamodb error"
+    },
+    "Item Deleted":{
+      "Type": "Pass",
+      "End": true
+    },
+    "Item Not Deleted":{
+      "Type": "Fail",
+      "Error": "Item Delete Fail",
+      "Cause": "Dynamodb error"
+    },
+    "Item Updated":{
+      "Type": "Pass",
+      "End": true
+    },
+    "Item Not Updated":{
+      "Type": "Fail",
+      "Error": "Item Update Fail",
+      "Cause": "Dynamodb error"
+    },
+    "Default":{
+      "Type": "Pass",
+      "End": true
     }
   }
 }
